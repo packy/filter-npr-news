2087ba1b8d898f915c14cdc02e07bb5f4ca24afa
Packy Anderson <packy@cpan.org>
2017-12-11 16:40:26 -0500

    Add a lot of comments to the Nina Totenberg feed rewriter.

--- convert-npr-rss-to-podcast.py
+++ convert-npr-rss-to-podcast.py
@@ -3,26 +3,30 @@
 # learning Python, why not use it to rewrite NPR's RSS feed for Nina Totenberg
 # so it's a podcast again?
 
+feedurl = 'http://www.npr.org/rss/rss.php?id=2101289'
+xmlfile = '/tmp/nina.xml'
+
 from bs4 import BeautifulSoup  # for parsing the HTML of the articles
 from lxml import etree
 import subprocess
 import sys, urllib
 
-feedurl = 'http://www.npr.org/rss/rss.php?id=2101289'
-xmlfile = '/tmp/nina.xml'
-
+# download the XML file from the URL and write it to a file
 response = urllib.urlopen(feedurl)
 data = response.read()
 outFile = open(xmlfile, 'w')
 outFile.write(data)
 outFile.close()
 
+# now use the lxml.etree parser to parse it
 doc = etree.parse(xmlfile)
 ns = {'content':'http://purl.org/rss/1.0/modules/content/'}
 
+# replace whatever the podcast image is with my own image
 for image in doc.xpath('/rss/channel/image/url'):
     image.text = 'http://packy.dardan.com/npr/ninatotenbergtile_sq.png'
 
+# loop over each item in the XML file
 for item in doc.xpath('/rss/channel/item'):
 
     # the content is CDATA coming in, but unless I explicitly make it
@@ -48,9 +52,10 @@ for item in doc.xpath('/rss/channel/item'):
     except:
         pass
 
-# re-write the XML to a file!
+# re-write the XML to the file we got it from
 outFile = open(xmlfile, 'w')
 doc.write(outFile)
 
+# copy the file up to my webserver so my phone can get it
 p = subprocess.Popen([ 'scp', xmlfile, 'dardanco@www.dardan.com:www/packy/npr/' ])
 sts = p.wait
